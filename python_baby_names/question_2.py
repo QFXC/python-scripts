2b661c1f94786a88f67edbc852ea3f3e7339b9f5
Tom Mazurek <tom@quantfxcapital.com>
2021-03-07 14:55:40 -0600

    Add excel exporting feature to python_baby_names question 2

--- python_baby_names/question_2.py
+++ python_baby_names/question_2.py
@@ -1,13 +1,14 @@
 import os
 import mixins
-import pandas
+import pandas as pd
 import settings
 
 from bs4 import BeautifulSoup
-# from IPython.display import display, HTML
 
 
 NAMES_IN_REPORT = ["Ryan", "Ben", "Eugene"]
+EXCEL_FILENAME = 'report.xlsx'
+EXCEL_SHEETNAME = 'Great Report'
 
 
 class Script(mixins.BabyNamesMixin):
@@ -15,14 +16,23 @@ class Script(mixins.BabyNamesMixin):
     This script details the rankings over the available years for specific
     baby names.
     """
-
-    def __init__(self, names_in_report: list):
+    def __init__(self, names_in_report: list, excel_filename: str = '',
+                 excel_sheetname: str = ''):
         self.names_in_report = names_in_report
+        if not excel_filename:
+            excel_filename = os.path.basename(__file__).replace('.py', '_report.xlsx')
+        self.excel_filename = excel_filename
+        self.excel_sheetname = excel_sheetname
 
     def execute_report(self):
         columns = ['Year'] + self.names_in_report
-        male_df = pandas.DataFrame(columns=columns)
-        female_df = pandas.DataFrame(columns=columns)
+        male_df = pd.DataFrame(columns=columns)
+        male_df.columns = pd.MultiIndex.from_tuples(
+            zip(['Male Name Rankings Per Year', '', '', ''], male_df.columns))
+
+        female_df = pd.DataFrame(columns=columns)
+        female_df.columns = pd.MultiIndex.from_tuples(
+            zip(['Female Name Rankings Per Year', '', '', ''], female_df.columns))
 
         filenames, available_years = self.get_filename_info()
 
@@ -56,31 +66,48 @@ class Script(mixins.BabyNamesMixin):
                 female_name = tag.next_sibling.text.strip()
                 female_names[female_name] = rank_num
 
-            data_in_row = [year]
             # Example:
-            # https://www.geeksforgeeks.org/how-to-add-one-row-in-an-existing-pandas-dataframe/
+            # https://www.geeksforgeeks.org/how-to-add-one-row-in-an-existing-pd-dataframe/
             # Append a new row to the male's table.
             male_df.loc[len(male_df.index)] = \
-                self.append_data_to_row(male_names, data_in_row)
+                self.add_data_to_row(male_names, [year])
 
             # Append a new row to the female's table.
             female_df.loc[len(female_df.index)] = \
-                self.append_data_to_row(female_names, data_in_row)
+                self.add_data_to_row(female_names, [year])
             html_file.close()
 
-        print('Male Names Ranking Per Year')
-        print(male_df.to_string())
-        print('Female Names Ranking Per Year')
-        print(female_df.to_string())
+        self.save_to_excel([male_df, female_df])
 
-    def append_data_to_row(names: dict, new_row: list = []) -> list:
+    def add_data_to_row(self, names: dict, new_row_data: list = []) -> list:
         for name in self.names_in_report:
             try:
                 rank = names[name]
             except KeyError:
                 rank = 'N/A'
-            new_row.append(rank)
-        return new_row
+            new_row_data.append(rank)
+        return new_row_data
 
+    def save_to_excel(self, dataframes):
+        output_path = (
+            os.path.dirname(os.path.abspath(__file__)) +
+            '\\' +
+            self.excel_filename
+        )
+        writer = pd.ExcelWriter(output_path, engine='xlsxwriter')
+        row = 0
+        for df in dataframes:
+            df.index = range(len(df))
+            df.to_excel(
+                writer,
+                sheet_name=self.excel_sheetname,
+                startrow=row,
+                startcol=0,
+            )
+            row = row + len(df.index) + 2
+        writer.save()
 
-Script(NAMES_IN_REPORT).execute_report()
+Script(
+    NAMES_IN_REPORT,
+    excel_sheetname=EXCEL_SHEETNAME,
+).execute_report()
